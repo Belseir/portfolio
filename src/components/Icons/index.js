18de951f885b10795c8b4a461cfbba98125f18a7
FutrykFabricio <ffutryk52@gmail.com>
2023-09-17 16:42:57 -0300

    feat: Add Icons

---
+++ src/components/Icons/index.js
@@ -0,0 +1,5 @@
+export { default as Github } from "./Github.astro";
+export { default as Golang } from "./Golang.astro";
+export { default as React } from "./React.astro";
+export { default as Steam } from "./Steam.astro";
+export { default as TypeScript } from "./TypeScript.astro";
